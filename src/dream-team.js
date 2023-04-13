4112d98b6a063f779b7590f6465a434bdc482b29
Lizaveta Zhydko <lizavetazhydko@gmail.com>
2023-04-13 20:54:36 +0200

    feat: done 5 functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const letters = members
+    .filter((name) => typeof name === 'string')
+    .map((name) => name.trim()[0].toUpperCase())
+    .sort();
+
+  return letters.join('');
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) {
+      return 0;
+    }
+    
+    let maxDepth = 1;
+    for (let i = 0; i < arr.length; i++) {
+      const depth = this.calculateDepth(arr[i]) + 1;
+      if (depth > maxDepth) {
+        maxDepth = depth;
+      }
+    }
+    return maxDepth;
   }
 }
 
