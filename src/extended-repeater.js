aad62aa4255bfd586a87a1b91f82e3011be7c451
Lizaveta Zhydko <lizavetazhydko@gmail.com>
2023-04-16 12:34:39 +0200

    feat: done 3 functions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let addition = '';
+  
+  if (options.hasOwnProperty('addition')) {
+    addition = options.addition;
+  }
+  
+  if (typeof addition !== 'string') {
+    addition = String(addition);
+  }
+  
+  let additionRepeatTimes = 1;
+  
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  
+  let additionSeparator = '|';
+  
+  if (options.hasOwnProperty('additionSeparator')) {
+    additionSeparator = options.additionSeparator;
+  }
+  
+  for (let i = 1; i <= options.repeatTimes; i++) {
+    result += String(str);
+    
+    for (let j = 1; j <= additionRepeatTimes; j++) {
+      result += addition;
+      
+      if (j < additionRepeatTimes) {
+        result += additionSeparator;
+      }
+    }
+    
+    if (i < options.repeatTimes) {
+      result += options.separator || '+';
+    }
+  }
+  
+  return result;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr parameter must be an instance of the Array!');
+  }
+  
+  const transformedArr = [];
+  let excludeNext = false;
+  let duplicateNext = false;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (excludeNext) {
+      excludeNext = false;
+      continue;
+    }
+    
+    if (duplicateNext && i < arr.length - 1) {
+      transformedArr.push(arr[i], arr[i+1]);
+      duplicateNext = false;
+      continue;
+    }
+    
+    if (arr[i] === '--discard-next') {
+      excludeNext = true;
+    } else if (arr[i] === '--discard-prev') {
+      if (transformedArr.length > 0) {
+        transformedArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      duplicateNext = true;
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0 && arr[i-1] !== '--discard-next') {
+        transformedArr.push(arr[i-1]);
+      }
+    } else {
+      transformedArr.push(arr[i]);
+    }
+  }
+  
+  return transformedArr;
 }
 
 module.exports = {
